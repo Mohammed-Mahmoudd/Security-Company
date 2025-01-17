bf539a191702aa76b70c95c21a8b30ab1b1dc63b
Mohammed Mahmoud Abd elhady <gamermada2@gmail.com>
2025-01-17 16:38:05 +0200

    updates

--- js/index.js
+++ js/index.js
@@ -1,28 +1,6 @@
-const observeElements = () => {
-    const elements = document.querySelectorAll('.animate-on-scroll');
-    const options = {
-      threshold: 0.5 
-    };
-  
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('animate-on-scroll-delay');
-          observer.unobserve(entry.target); 
-        }
-      });
-    }, options);
-  
-    elements.forEach(element => observer.observe(element));
-  };
-  
-  document.addEventListener('DOMContentLoaded', observeElements);
-  
-
 const backToTopButton = document.getElementById('backToTop');
-
 window.onscroll = function () {
-  if (document.body.scrollTop > 125 || document.documentElement.scrollTop > 125) {
+  if (document.body.scrollTop > 125) {
     backToTopButton.style.display = 'block';
   } else {
     backToTopButton.style.display = 'none';
@@ -35,4 +13,62 @@ backToTopButton.onclick = function () {
     behavior: 'smooth'
   });
 };
+
+const form = document.getElementById("form")
+const errorName = document.getElementById("errorName")
+const impname = document.getElementById("name")
+const errorEmail = document.getElementById("errorEmail")
+const inpemail = document.getElementById("email")
+const inpmessage = document.getElementById("message")
+const errorMessage = document.getElementById("errorMessage")
+const btn = document.getElementById("send")
+
+
+const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/
+const nameRegex = /^[a-zA-Z ]{2,}$/
+const messageRegex = /^[A-Za-z0-9_%$@ ]{15,}$/
+
+form.addEventListener("submit" , function(prevent) {
+  if(inpemail.value === "" || impname.value === "" || inpmessage.value === "") {
+    prevent.preventDefault()
+    errorEmail.innerHTML = ""
+    errorName.innerHTML = ""
+    errorMessage.innerHTML = ""
+    errorMessage.innerHTML = "Please enter value for all fields" 
+    errorMessage.style.color = "red"
+    errorMessage.style.fontSize = "12px"
+    
+  }else {
+    errorMessage.innerHTML = "" 
+    impname.value = impname.value.trim()
+    if(!nameRegex.test(impname.value)) {
+      prevent.preventDefault()
+      errorName.innerHTML = "Please enter a valid Name without numbers and above 1 character | John "
+      errorName.style.color = "red"
+      errorName.style.fontSize = "12px"
+    }else {
+      errorName.innerHTML =""
+      if(!emailRegex.test(inpemail.value)) {
+        prevent.preventDefault()
+        errorEmail.innerHTML = "Please Enter A valid Email | exampleEmail@gmail.com"
+        errorEmail.style.color = "red"
+        errorEmail.style.fontSize = "12px"
+        console.log(messageRegex.test(inpmessage.value))
+        console.log(inpmessage.value)
+      
+    }else {
+      errorEmail.innerHTML = ""
+      if(!messageRegex.test(inpmessage.value)) {
+        prevent.preventDefault()
+        errorMessage.textContent = "Please enter a message above 14 character "
+        errorMessage.style.color = "red"
+        errorMessage.style.fontSize = "12px"
+      }else {
+      errorMessage.textContent = ""
+      
+    }
+    }
+  }
+  }
+})
 
